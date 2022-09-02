3bc6f16aef23a772043e020f8e4a3afddb43a85e
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-09-02 16:22:20 +0200

    end exrcise and split js beetwen front and back

---
+++ resources/js/views/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <a href="/admin">Admin Page</a>
+    <h1>la mia home per tutti</h1> 
+  </div> 
+</template>
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')
 ->namespace('Admin')
-->name('Admin.')
+->name('admin.')
 ->prefix('admin')
 ->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('{any?}', function () {
+    return view('guest.home');
 });
 
-Auth::routes();
